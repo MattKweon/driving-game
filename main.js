eb4a05b86a6c8cd5f7f9ba6faaca1035293ac368
Matthew Kweon <kweon.matt@gmail.com>
2022-09-21 22:35:26 +0000

    revised issue-2

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ var $img = document.querySelector('img');
 
 document.addEventListener('keydown', function (event) {
   if (event.keyCode === 39) {
-    $img.style.transform = 'rotate(90deg)';
+    $img.className = 'turn-90';
   } else if (event.keyCode === 40) {
-    $img.style.transform = 'rotate(180deg)';
+    $img.className = 'turn-180';
   } else if (event.keyCode === 37) {
-    $img.style.transform = 'rotate(270deg)';
+    $img.className = 'turn-90';
   } else if (event.keyCode === 38) {
-    $img.style.transform = 'rotate(360deg)';
+    $img.className = '';
   }
 });
